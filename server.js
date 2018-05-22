e7a8b9a9479b1b98377c089c2751a386423270a1
Jahdiel Vargas <jahdielvargasmusic@gmail.com>
2018-05-21 22:20:16 -0400

    Añadiendo Vistas y sirviendo archivos estaticos

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 var express = require('express')
 var app = express()
 
+app.set('view engine', 'pug')
+
+app.use(express.static('public'))
+
 app.get('/', function(req, res) {
-    res.send('Hello Word!')
+    res.render('index')
 })
 
 app.listen(3000, function(err){
     if(err) return console.log('Error'), process.exit(1)
-
     console.log('Escuchando en el puerto 3000')
 })
