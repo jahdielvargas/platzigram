0491c18517766ad44b056f647eef9d977a2f0489
Jahdiel Vargas <jahdielvargasmusic@gmail.com>
2018-05-25 00:56:49 -0400

    Añadiendo Timeline para nuestros usuarios

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -1,6 +1,11 @@
 var page = require('page')
+var empty = require('empty-element')
+var template = require('./template')
+// var title = require('title')
 
 page('/', function (ctx, next) {
+  // title('Platzigram | Sign Up')
+  document.title= 'Platzigram'
   var main = document.getElementById('main-container')
-  main.innerHTML= '<a href="/signup">Sign Up</a>'
+  empty(main).appendChild(template)
 })
