be785b17fc8985b06444d7760c8017857188a07c
Jahdiel Vargas <jahdielvargasmusic@gmail.com>
2018-05-29 23:17:25 -0400

    Añadiendo boton de subir imagen y enviarla al servidor

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 var express = require('express')
+var multer  = require('multer')
+var ext = require('file-extension')
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+      cb(null, +Date.now() + '.'+ ext(file.originalname))
+    }
+  })
+   
+var upload = multer({ storage: storage }).single('picture')
+
 var app = express()
 
 app.set('view engine', 'pug')
 
 app.use(express.static('public'))
 
-app.get(['/', '/signup', '/signin'], function(req, res) {
+app.get(['/'], function(req, res) {
     res.render('index', { title: 'Platzigram'})
 })
 
@@ -45,6 +58,14 @@ app.get('/api/pictures', function (req, res){
     }, 2000)
 })
 
+app.post('/api/pictures', function (req, res) {
+    upload(req, res, function (err){
+        if (err) {
+            return res.send(500, 'Error Uploading File :(')
+        }
+        res.send('File Uploaded :)')
+    })
+})
 app.listen(3000, function(err){
     if(err) return console.log('Error'), process.exit(1)
     console.log('Escuchando en el puerto 3000')
--- src/signin/index.js
+++ src/signin/index.js
@@ -1,11 +1,8 @@
 var page = require('page')
 var empty = require('empty-element')
 var template = require('./template')
-// var title = require('title')
 
 page('/signin', function (ctx, next) {
-  // title('Platzigram | Sign In')
-  document.title = 'Platzigram | Sign In'
   var main = document.getElementById('main-container')
   empty(main).appendChild(template)
 })
